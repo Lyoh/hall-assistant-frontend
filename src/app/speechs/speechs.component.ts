8b156cd62f3824748e085e567bf70f06fb53099b
Riubbe S. Oliveira <rb.oliveira@hotmail.com>
2017-12-01 17:58:43 -0200

    Fixing the footer

--- src/app/speechs/speechs.component.ts
+++ src/app/speechs/speechs.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Speech } from './shared/speech.model';
+import { SpeechsService } from './speechs.service';
+
 @Component({
   selector: 'app-speechs',
   templateUrl: './speechs.component.html',
   styleUrls: ['./speechs.component.css']
 })
 export class SpeechsComponent implements OnInit {
-
-  constructor() { }
+  public speechs: Array<Speech>;
+  constructor(private speechService: SpeechsService) {
+    this.speechs = null;
+   }
 
   ngOnInit() {
+    this.speechService.getAll()
+      .subscribe(
+        (speechs) => this.speechs = speechs,
+        () => this.handlesError()
+      );
+  }
+
+  private handlesError() {
+    console.log('Tratar o erro aqui');
   }
 
 }
